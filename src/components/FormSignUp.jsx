3e5a92628ffcdcec4a09193648f4a8e530e764ab
OOGD <oogd17@gmail.com>
2023-06-12 13:13:43 -0600

    Terminando funcion para validar campo nombre

--- src/components/FormSignUp.jsx
+++ src/components/FormSignUp.jsx
@@ -11,6 +11,21 @@ const FormSignUp = (props) => {
     const [prom, setProm] = useState(true);
     const [nov, setNov] = useState(false);
 
+    const [errors, setErrors] = useState({
+        name : {
+            error: false,
+            message: ''
+        },
+    });
+
+    const validarNombre = (nombre) => {
+        if(nombre.length >= 3) {
+            return {name: {error: false, message: ''}}
+        } else {
+            return {name: {error: true, message: 'Debe tener al menos 3 caracteres.'}}
+        }
+    }
+
     return <form onSubmit={(e) =>{
         e.preventDefault();
         handleSubmit({name, lastName, email, prom, nov});
@@ -18,10 +33,15 @@ const FormSignUp = (props) => {
     
         <TextField id='name' label='Nombre' variant="outlined" fullWidth margin="normal"
         onChange={(e)=> {setName(e.target.value);}}
-        value={name}/>
+        value={name}
+        error={errors.name.error}
+        helperText={errors.name.error ? errors.name.message : ''}
+        onBlur={(e) => setErrors(validarNombre(e.target.value))}/>
+
         <TextField id='lastName' label='Apellidos' variant="outlined" fullWidth margin="normal"
         onChange={(e)=> {setLastName(e.target.value);}}
         value={lastName}/>
+
         <TextField id='email' label='Email' variant="outlined" fullWidth margin="normal"
         onChange={(e)=> {setEmail(e.target.value);}}
         value={email}/>
